88ef84b60cf4fa592a2f98d15a4931d7e09e1106
jeichlcof <74484217+jeichlcof@users.noreply.github.com>
2020-12-04 16:02:36 +0900

    finished

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-const number = document.querySelector("button");
 const form = document.querySelector("form");
 let input = form.querySelector("input");
+let calculator = "";
+let preval = "";
 
 function output(num) {
-  input.value = input.value + num;
+  calculator = calculator + num;
+  preval = calculator.slice(0, -1);
+  const lastNumber = calculator.charAt(calculator.length - 1);
+  if (
+    lastNumber === "*" ||
+    lastNumber === "/" ||
+    lastNumber === "+" ||
+    lastNumber === "-"
+  ) {
+    input.value = "";
+    input.value = eval(calculator.slice(0, -1));
+  }
+
+  if (num !== "*" && num !== "+" && num !== "/" && num !== "-") {
+    if (
+      preval.charAt(preval.length - 1) === "*" ||
+      preval.charAt(preval.length - 1) === "/" ||
+      preval.charAt(preval.length - 1) === "+" ||
+      preval.charAt(preval.length - 1) === "-"
+    ) {
+      input.value = "";
+    }
+    input.value = input.value + num;
+  }
 }
 
 function equal() {
-  let exp = input.value;
-  exp = eval(exp);
-  input.value = exp;
+  input.value = eval(calculator);
 }
 
 function reset() {
   input.value = "";
+  calculator = "";
 }
